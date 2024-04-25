2434075460a82780225e3f944a647688b1f08edc
annieherieau <annie.herieau@gmail.com>
2024-04-25 19:22:56 +0200

    _OMBD_APIKEY

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { _APIKEY } from "./env.js";
 
 const _URL = "https://www.omdbapi.com";
 const NO_POSTER = "images/placeholder.png";
-const _apikey = _APIKEY !== '' ? _APIKEY : ${{ secrets._OMBD_APIKEY }};
+const _apikey = _APIKEY !== '' ? _APIKEY : process.env._OMBD_APIKEY;
 // Animations
 AOS.init();
 
